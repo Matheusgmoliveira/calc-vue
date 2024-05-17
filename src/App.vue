cb24973e7a998f345d48015987197be81ce95e0a
Matheus Gonçalves <matheusgoncalves1970@gmail.com>
2024-05-17 05:21:49 -0300

    subindo o projeto finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,92 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {reactive, watch} from 'vue';
+
+const calculadora= reactive({
+    filtro:'somar',
+    num1:0,
+    num2:0,
+    resultado:0
+  
+})
+
+
+const funcaoSomar = () => {
+  const somar = parseFloat(calculadora.num1) + parseFloat(calculadora.num2);
+  calculadora.resultado = somar;
+}
+
+const funcaoMultiplicar = () => {
+  const multiplicar = parseFloat(calculadora.num1) * parseFloat(calculadora.num2);
+  calculadora.resultado = multiplicar;
+}
+
+const funcaoDividir = () => {
+  if (calculadora.num2 != 0) {
+    const dividir = parseFloat(calculadora.num1) / parseFloat(calculadora.num2);
+    calculadora.resultado = dividir;
+  } else {
+    calculadora.resultado = 'Erro: Divisão por zero';
+  }
+}
+
+const funcaoSubtrair = () => {
+  const subtrair = parseFloat(calculadora.num1) - parseFloat(calculadora.num2);
+  calculadora.resultado = subtrair;
+}
+
+const operador = () =>{
+  const{filtro} = calculadora;
+  switch(filtro){
+    case 'somar':
+      return funcaoSomar();
+    case 'multiplicar':
+      return funcaoMultiplicar();
+    case 'dividir':
+      return funcaoDividir();
+    case 'subtrair':
+      return funcaoSubtrair();
+    default:
+      return 0
+  }
+}
+
+const calcular = () =>{
+  operador();
+}
+
+watch ( () => [calculadora.num1, calculadora.num2, calculadora.filtro],calcular)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <h1 class="p-5 mb-4 mt-4 bg-light rounded-3">Calculadora</h1>
+    <div class="row">
+      <div class="col-md-3">
+        <input @keyup ="evento => calculadora.num1 = evento.target.value" type="number" name="" id="" class="form-control">
+      </div>
+      <div class="col-md-3">
+        <input @keyup ="evento => calculadora.num2 = evento.target.value" type="number" name="" id="" class="form-control">
+      </div>
+      <div class="col-md-3">
+        <select  @change ="evento =>calculadora.filtro=evento.target.value"class="form-control">
+          <option value="somar">Somar</option>
+          <option value="multiplicar">Multiplicar</option>
+          <option value="subtrair">Subtrair</option>
+          <option value="dividir">Dividir</option>
+        </select>
+      </div>
+      
+      <div class="row">
+        <div class="mt-3 col-md-2">
+          <p class="form-control" >{{ calculadora.resultado }}</p>
+          
+        </div>
+        
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
